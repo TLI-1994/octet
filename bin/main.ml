2a7b0cc0ff4378c440a13123ea3fdeacbe653d3d
Sanjit Basker <sb866@cornell.edu>
2022-03-12 11:27:08 -0500

    handle resizing the terminal!

--- bin/main.ml
+++ bin/main.ml
@@ -17,6 +17,7 @@ let main =
         | `Key (`ASCII 'C', [ `Ctrl ]) -> ()
         | _ -> loop t state)
     | `Key key -> update t (update_all key state)
+    | `Resize _ -> update t state
     | _ -> loop t state
   in
   let t = Term.create () in
@@ -24,15 +25,3 @@ let main =
   Term.release t
 
 let () = main
-
-(* let init = Obuffer.empty let img _ = Orender.image_of_buffer let img2
-   t buffer = Obuffer.to_image buffer 0 (Term.size t) true
-
-   let main = let rec update t state = Term.image t (img2 t state); loop
-   t state and loop t state = match Term.event t with | `Key (`ASCII
-   'X', [ `Ctrl ]) -> ( match Term.event t with | `Key (`ASCII 'C', [
-   `Ctrl ]) -> () | _ -> loop t state) | `Key key -> update t
-   (Obuffer.update_on_key state key) | _ -> loop t state in let t =
-   Term.create () in update t init; Term.release t
-
-   let () = main *)
