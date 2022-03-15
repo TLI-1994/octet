d15b2585137dda3a9b81e0e0c29bdd0064d5c6b0
Sidhanth Holalkere <sidhanth.holalkere@gmail.com>
2022-03-15 15:33:36 -0400

    Added split_at_n tests

--- test/octet.ml
+++ test/octet.ml
@@ -1,4 +1,8 @@
 open OUnit2
+open Util
+
+let string_of_string_list (input : string list) : string =
+  input |> List.map (fun s -> s ^ ", ") |> List.fold_left ( ^ ) ""
 
 let insert_test
     (name : string)
@@ -6,11 +10,20 @@ let insert_test
     (i : int)
     (c : char)
     (expected_output : string) : test =
-  let open Util in
   name >:: fun _ ->
   assert_equal expected_output
     (insert_at_n input_line i c)
     ~printer:String.escaped
+
+let split_test
+    (name : string)
+    (input_line : string)
+    (i : int)
+    (expected_output : string list) : test =
+  name >:: fun _ ->
+  assert_equal expected_output
+    (split_at_n input_line i)
+    ~printer:string_of_string_list
 
 (** TODO: test Obuffer.break_line. *)
 
@@ -27,6 +40,13 @@ let obuffer_tests =
       5 't' "hellot world\n";
     insert_test "insert into end of newline-terminated string"
       "hello world\n" 11 '!' "hello world!\n";
+    split_test "split in middle of string" "hello world" 5
+      [ "hello"; " world" ];
+    split_test "split at beginning of string" "hello world" 0
+      [ ""; "hello world" ];
+    split_test "split at end of string" "hello world" 11
+      [ "hello world"; "" ];
+    split_test "split empty string" "" 0 [ ""; "" ];
   ]
 
 let tests = "test suite for project" >::: List.flatten [ obuffer_tests ]
