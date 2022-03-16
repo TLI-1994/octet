bce1104a947e638e5ea35b449281cca3e228ac0a
Sanjit Basker <sb866@cornell.edu>
2022-03-16 18:29:45 -0400

    Added authors compilation unit

---
+++ src/authors.mli
@@ -0,0 +1,10 @@
+(** CS 3110 Spring 2022 Final Project (MS1)
+
+    @author Sanjit Basker (sb866)
+    @author Sidhanth Holalkere (sh844)
+    @author Tianjiao Li (tl787) *)
+
+
+ val hours_worked : int
+ (** [hours_worked] is the number of hours we worked on this assignment. *)
+ 
