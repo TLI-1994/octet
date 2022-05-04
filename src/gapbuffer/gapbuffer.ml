5104af7284d6d35c541beda1c200cdb5b1365912
Tianjiao Li <tl787@cornell.edu>
2022-05-03 20:34:10 -0400

    Fix bugs in `left`, `right`, `insert` and `delete`

--- src/gapbuffer/gapbuffer.ml
+++ src/gapbuffer/gapbuffer.ml
@@ -1,11 +1,10 @@
 module GapBuffer = struct
   type t = {
-    main_array : char array;
+    mutable main_array : char array;
     mutable gap_len : int;
     mutable gap_left : int;
     mutable gap_right : int;
-    gap_incr : int;
-    mutable cur : int;
+    mutable num_of_char : int;
   }
 
   let init buffer_length gap_length : t =
@@ -16,11 +15,19 @@ module GapBuffer = struct
       gap_len;
       gap_left;
       gap_right = gap_left + gap_len - 1;
-      gap_incr = gap_length;
-      cur = 0;
+      num_of_char = 0;
     }
 
-  let make s i = raise (Failure "Unimplemented")
+  let make str len =
+    let num_of_char = String.length str in
+    let main_array =
+      Array.init len (fun i ->
+          if i < num_of_char then String.get str i else '_')
+    in
+    let gap_len = len in
+    let gap_left = num_of_char in
+    let gap_right = len - 1 in
+    { main_array; gap_len; gap_left; gap_right; num_of_char }
 
   let to_string gb =
     let buf =
@@ -39,14 +46,14 @@ module GapBuffer = struct
     for i = 0 to pos - 1 do
       gb.main_array.(i) <- temp_main_array.(i)
     done;
-    for i = 0 to gb.gap_incr - 1 do
+    for i = 0 to gb.gap_len - 1 do
       gb.main_array.(pos + i) <- '_'
     done;
     for i = pos to Array.length temp_main_array - 1 do
-      gb.main_array.(gb.gap_incr + i) <- temp_main_array.(i)
+      gb.main_array.(gb.gap_len + i) <- temp_main_array.(i)
     done;
-    gb.gap_right <- gb.gap_right + gb.gap_incr;
-    gb.gap_len <- gb.gap_len + gb.gap_incr
+    gb.gap_right <- gb.gap_right + gb.gap_len;
+    gb.gap_len <- 2 * gb.gap_len
 
   let mv_left gb pos =
     while gb.gap_left <> pos do
@@ -67,33 +74,90 @@ module GapBuffer = struct
   let mv_gap gb pos =
     if pos < gb.gap_left then mv_left gb pos else mv_right gb pos
 
+  let resize gb =
+    let old_length = Array.length gb.main_array in
+    let new_main_array =
+      Array.init (2 * old_length) (fun i ->
+          if i < old_length then gb.main_array.(i) else '_')
+    in
+    gb.main_array <- new_main_array
+
   let insert_string gb instring pos =
+    if
+      String.length instring + gb.num_of_char >= gb.gap_len
+      && Array.length gb.main_array < 2 * gb.gap_len
+      (* Array.length gb.main_array < gb.num_of_char + gb.gap_len +
+         String.length instring *)
+    then resize gb
+    else ();
     if gb.gap_left <> pos then mv_gap gb pos else ();
     for i = 0 to String.length instring - 1 do
       if gb.gap_left = gb.gap_right then grow gb (pos + i) else ();
       gb.main_array.(pos + i) <- instring.[i];
-      gb.gap_left <- gb.gap_left + 1
+      gb.gap_left <- gb.gap_left + 1;
+      gb.num_of_char <- gb.num_of_char + 1
     done
 
   let delete_at_pos gb pos =
     if gb.gap_left <> pos then mv_gap gb pos else ();
     gb.gap_right <- gb.gap_right + 1;
-    gb.main_array.(gb.gap_right) <- '_'
+    gb.main_array.(gb.gap_right) <- '_';
+    gb.num_of_char <- gb.num_of_char - 1
 
-  let left gb = gb.cur <- max (gb.cur - 1) 0
-  let right gb = gb.cur <- min (gb.cur + 1) (gb.gap_len - 1)
-  let insert gb c = insert_string gb (Char.escaped c) gb.cur
-  let delete gb = delete_at_pos gb gb.cur
+  let left gb =
+    if gb.gap_left <> 0 then mv_left gb (gb.gap_left - 1) else ()
 
-  (* let make s ll = *)
+  let right gb =
+    if gb.gap_left <> gb.num_of_char then mv_right gb (gb.gap_left + 1)
+    else ()
+
+  let insert gb c = insert_string gb (Char.escaped c) gb.gap_left
+
+  let delete gb =
+    if gb.gap_left <> 0 then delete_at_pos gb (gb.gap_left - 1) else ()
+
+  let string_of_buffer gb =
+    let output = ref "" in
+    for i = 0 to Array.length gb.main_array - 1 do
+      if i >= gb.gap_left && i <= gb.gap_right then
+        output := !output ^ Char.escaped '='
+      else output := !output ^ Char.escaped gb.main_array.(i)
+    done;
+    !output
 end
 
-(* open GapBuffer
+open GapBuffer
 
-   let () = let gb = init 50 10 in print_endline (to_string gb);
-   insert_string gb "octet" 0; print_endline (to_string gb);
-   delete_at_pos gb 4; print_endline (to_string gb); insert_string gb
-   "OCTET" 0; print_endline (to_string gb); insert_string gb "*" 0;
-   print_endline (to_string gb); insert gb '*'; print_endline (to_string
-   gb); insert gb '&'; print_endline (to_string gb); right gb; right gb;
-   right gb; insert gb '#'; print_endline (to_string gb) *)
+let () =
+  (* let gb = init 10 3 in *)
+  let gb = make "yes we can" 15 in
+  for i = 0 to 30 do
+    insert gb (char_of_int ((i mod 26) + int_of_char 'a'));
+    (* print_endline (string_of_buffer gb); *)
+    print_endline (to_string gb)
+  done;
+  for i = 0 to 10 do
+    left gb;
+    (* print_endline (string_of_buffer gb); *)
+    print_endline (to_string gb)
+  done;
+  for i = 0 to 10 do
+    delete gb;
+    (* print_endline (string_of_buffer gb); *)
+    print_endline (to_string gb)
+  done;
+  for i = 0 to 20 do
+    right gb;
+    (* print_endline (string_of_buffer gb); *)
+    print_endline (to_string gb)
+  done;
+  for i = 0 to 40 do
+    left gb;
+    (* print_endline (string_of_buffer gb); *)
+    print_endline (to_string gb)
+  done;
+  for i = 0 to 40 do
+    insert gb (char_of_int ((i mod 26) + int_of_char 'a'));
+    (* print_endline (string_of_buffer gb); *)
+    print_endline (to_string gb)
+  done
