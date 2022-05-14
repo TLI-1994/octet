36fcf832cb60f88b7e21c04009e6b352ff3fd280
Tianjiao Li <tl787@cornell.edu>
2022-05-14 16:48:10 -0400

    Add `cursor_line` field to `Filebuffer.t`

--- src/filebuffer.ml
+++ src/filebuffer.ml
@@ -23,6 +23,7 @@ module Make (LineBuffer : Obuffer.MUT_BUFFER) : MUT_FILEBUFFER = struct
     mutable cursor_pos : int;
     mutable cursor_pos_cache : int;
     mutable desc : string;
+    mutable cursor_line : int;
   }
   (** AF: buffers for the lines in the text file before and including
       the line with the cursor are in [front], in reverse order; buffers
@@ -41,6 +42,7 @@ module Make (LineBuffer : Obuffer.MUT_BUFFER) : MUT_FILEBUFFER = struct
       cursor_pos = 0;
       cursor_pos_cache = 0;
       desc = "data/Empty Buffer";
+      cursor_line = 0;
     }
 
   let insert_char fb c =
@@ -64,6 +66,7 @@ module Make (LineBuffer : Obuffer.MUT_BUFFER) : MUT_FILEBUFFER = struct
             (Util.split_at_n s fb.cursor_pos)
         in
         fb.front <- List.rev_append l t;
+        fb.cursor_line <- fb.cursor_line + 1;
         fb.cursor_pos <- 0;
         fb.cursor_pos_cache <- 0;
         fb
@@ -74,6 +77,7 @@ module Make (LineBuffer : Obuffer.MUT_BUFFER) : MUT_FILEBUFFER = struct
     | [ _ ] -> fb
     | h1 :: h2 :: t ->
         LineBuffer.move_to h2 fb.cursor_pos_cache;
+        fb.cursor_line <- fb.cursor_line - 1;
         fb.cursor_pos <-
           min fb.cursor_pos_cache (LineBuffer.content_size h2);
         fb.front <- h2 :: t;
@@ -83,16 +87,13 @@ module Make (LineBuffer : Obuffer.MUT_BUFFER) : MUT_FILEBUFFER = struct
   let mv_down fb =
     match fb.back with
     | [] -> fb
-    | [ x ] ->
-        fb.front <- x :: fb.front;
-        fb.back <- [];
-        fb
-    | h1 :: h2 :: t ->
-        LineBuffer.move_to h1 fb.cursor_pos_cache;
+    | h :: t ->
+        LineBuffer.move_to h fb.cursor_pos_cache;
+        fb.cursor_line <- fb.cursor_line + 1;
         fb.cursor_pos <-
-          min fb.cursor_pos_cache (LineBuffer.content_size h1);
-        fb.front <- h1 :: fb.front;
-        fb.back <- h2 :: t;
+          min fb.cursor_pos_cache (LineBuffer.content_size h);
+        fb.front <- h :: fb.front;
+        fb.back <- t;
         fb
 
   let mv_left fb =
@@ -160,4 +161,84 @@ module Make (LineBuffer : Obuffer.MUT_BUFFER) : MUT_FILEBUFFER = struct
     let new_buffer = from_file temp_path in
     Sys.command ("rm " ^ temp_path) |> ignore;
     { new_buffer with desc = fb.desc }
+
+  (* open Notty open Notty.Infix
+
+     let rec to_image (buffer : t) (top_line : int) ((width, height) :
+     int * int) (show_cursor : bool) = let height = height - 1 in let
+     width = width - 5 in let contents = contents buffer in let
+     buffer_contents = let l = List.length contents in if l >= height
+     then contents else contents @ List.map (fun _ -> "") (Util.from 0
+     (height - l)) in let buffer_contents = List.map (fun s -> if
+     String.length s < width then s ^ String.make (width - String.length
+     s) ' ' else s) buffer_contents in let line_nos = Util.from 0
+     (height - 1) |> List.map (fun d -> I.string A.(bg black ++ st
+     italic) (Printf.sprintf "% 3d " d)) |> I.vcat in let remaining =
+     list_from_nth buffer_contents top_line in let superimposed =
+     List.mapi (render_line buffer top_line show_cursor) remaining in
+     let widthcropped = I.vcat (List.map (wrap_to width) superimposed)
+     in let heightcropped = I.vcrop 0 (I.height widthcropped - height)
+     widthcropped in line_nos <|> heightcropped <-> modeline_to_image
+     buffer width
+
+     and list_from_nth lst = function | 0 -> lst | n -> list_from_nth
+     (List.tl lst) @@ (n - 1)
+
+     and wrap_to width img = let rec go off = I.hcrop off 0 img :: (if
+     I.width img - off > width then go (off + width) else []) in go 0 |>
+     I.vcat |> I.hsnap ~align:`Left width
+
+     and render_unselected (line : string) (active : bool) : image =
+     I.string (if active then A.(fg lightwhite ++ bg lightblack) else
+     A.empty) line
+
+     and render_selected (line : string) : image = I.string A.(bg
+     lightblue) line
+
+     and render_beginning_selected (line : string) (pos : int) (active :
+     bool) : image = let parts = Util.split_at_n line pos in
+     render_selected (List.nth parts 0) <|> render_unselected (List.nth
+     parts 1) active
+
+     and render_end_selected (line : string) (pos : int) (active : bool)
+     : image = let parts = Util.split_at_n line pos in render_unselected
+     (List.nth parts 0) active <|> render_selected (List.nth parts 1)
+
+     and render_selected_in_line (line : string) (s : int) (e : int)
+     (active : bool) : image = let parts = Util.split_at_n line e in
+     render_end_selected (List.nth parts 0) s active <|>
+     render_unselected (List.nth parts 1) active
+
+     and render_line_without_cursor (buffer : t) (absolute_line : int)
+     (line : string) (active : bool) = if buffer.mark_active then let
+     min_select = min buffer.cursor_line buffer.mark_line in let
+     max_select = max buffer.cursor_line buffer.mark_line in let
+     min_select_pos = min buffer.cursor_pos buffer.mark_pos in let
+     max_select_pos = max buffer.cursor_pos buffer.mark_pos in let
+     start_pos = if buffer.cursor_line < buffer.mark_line then
+     buffer.cursor_pos else buffer.mark_pos in let end_pos = if
+     buffer.cursor_line > buffer.mark_line then buffer.cursor_pos else
+     buffer.mark_pos in if min_select = max_select && absolute_line =
+     min_select then (* one line *) render_selected_in_line line
+     min_select_pos max_select_pos active else if absolute_line <
+     min_select then render_unselected line active else if absolute_line
+     > max_select then render_unselected line active else if
+     absolute_line = min_select then render_end_selected line start_pos
+     active else if absolute_line = max_select then
+     render_beginning_selected line end_pos active else render_selected
+     line else render_unselected line active
+
+     and render_line (buffer : t) (top_line : int) (show_cursor : bool)
+     (i : int) (elt : string) = let absolute_location = i + top_line in
+     if absolute_location = buffer.cursor_line && show_cursor then
+     add_cursor (render_line_without_cursor buffer absolute_location elt
+     show_cursor) buffer.cursor_pos else render_line_without_cursor
+     buffer absolute_location elt show_cursor
+
+     and modeline_to_image (buffer : t) (width : int) = I.string A.(fg
+     black ++ bg white) (buffer.desc ^ " Cursor Line: " ^ string_of_int
+     buffer.cursor_line ^ " Col: " ^ string_of_int buffer.cursor_pos ^
+     "\n Mark Line: " ^ string_of_int buffer.mark_line ^ " Col: " ^
+     string_of_int buffer.mark_pos) </> I.char A.(fg black ++ bg white)
+     ' ' width 1 *)
 end
