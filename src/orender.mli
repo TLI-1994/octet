4aad48765b1d23cb8970a90e39cb193d9a176255
Sidhanth Holalkere <sidhanth.holalkere@gmail.com>
2022-05-17 12:27:10 -0400

    updated comments in orender.mli

--- src/orender.mli
+++ src/orender.mli
@@ -2,6 +2,12 @@
 
 val image_of_string :
   (int * int) option -> int option -> string -> Notty.image
+(** [image_of_string (Some (hl_start, hl_end)) (Some cursor_loc) s]
+    returns a Notty image of [s] with syntax highlighting. If
+    [(hl_start, hl_end)] is specified, [s] will be rendered with the
+    characters from [hl_start] to [hl_end] being highlighted. If
+    [cursor_loc] is specified, a cursor will eb rendered at position
+    [cursor_loc].*)
 
 val make_line_numbers : int -> Notty.image
 (** [make_line_numbers h] creates an image of line numbers from [0] to
@@ -12,3 +18,5 @@ val crop_to : int * int -> Notty.image list -> Notty.image
     [height] with all the lines of text in [img_lst] *)
 
 val char_tags_of_string_debug : string -> string
+(** [char_tags_of_string_debug s] prepends each character in [s] with
+    what tag of [Keyword | Symbol | Number | Other] it belongs to *)
